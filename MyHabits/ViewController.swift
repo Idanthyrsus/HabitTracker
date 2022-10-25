1e887f22d25c5d1011a6b94775427fb3be172b2a
Alex Korchak <mocker-waves@mail.ru>
2022-10-25 10:12:27 +0300

    Initial Commit

---
+++ MyHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Alexander Korchak on 25.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
